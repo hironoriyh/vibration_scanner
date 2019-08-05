3b8b505f21b09d2c2963023d02650dcff00be0b0
hironoriyh <hironoriyh@gmail.com>
2019-08-06 08:25:56 +0900

    test

--- diff_frames.py
+++ diff_frames.py
@@ -1,39 +1,79 @@
 import cv2
+import numpy as np
+import matplotlib.pyplot as plt
 
-vid = cv2.VideoCapture("h80hz.avi")
+
+cap = cv2.VideoCapture("h80hz.avi")
+
+all_frame_nums  =  cap.get(cv2.CAP_PROP_FRAME_COUNT)
+
+print("num of frames", all_frame_nums)
+
 
 count = 0
+frame_nums = np.arange(1000)
 # while True:
-#     vid.grab()
+pixel_time = []
+
+
+mutable_object = {}
+
+cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+ret, frame = cap.read()
+fig= plt.imshow(frame)
+fig = plt.figure()
+def onclick(event):
+    print('you pressed', event.key, event.xdata, event.ydata)
+    X_coordinate = event.xdata
+    Y_coordinate = event.ydata
+    mutable_object['click'] = X_coordinate
+
+cid = fig.canvas.mpl_connect('button_press_event', onclick)
+lines, = plt.plot([1,2,3])
+plt.show()
+X_coordinate = mutable_object['click']
+print(X_coordinate)
+
+for frame_num in frame_nums:
+    # cap.grab()
+    print(frame_num)
+    cap.set(cv2.CAP_PROP_POS_FRAMES, frame_num)
+
+
+    # ret, frame = cap.retrieve()
+    ret, frame = cap.read()
+    snapshot = np.zeros(frame.shape, dtype=np.uint8)
+    # colorArray = np.zeros((COLOR_ROWS, COLOR_COLS, 3), dtype=np.uint8)
+    # print(frame[500][500])
+    pixel_time.append(frame[500][500])
+
+    if not ret:
+        break
+
+
+    # cv2.imshow("Test", frame)
+    # cv2.waitKey(1)
+
+print(pixel_time)
+
+# while(cap.isOpened()):
+#     if(count%100==0):
 #
-#     retval, image = vid.retrieve()
+#         ret, frame = cap.read()
 #
-#     if not retval:
+#         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#
+#         cv2.imshow('frame',gray)
+#
+#
+#         print(count)
+#
+#     else:
+#         continue
+#
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
 #         break
-#
-#
-#     cv2.imshow("Test", image)
-#     cv2.waitKey(1)
+#     count += 1
 
-
-while(vid.isOpened()):
-    if(count%100==0):
-
-        ret, frame = vid.read()
-
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-        cv2.imshow('frame',gray)
-
-
-        print(count)
-
-    else:
-        continue
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break        
-    count += 1
-
-vid.release()
+cap.release()
 cv2.destroyAllWindows()
