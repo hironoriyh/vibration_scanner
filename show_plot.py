85820969d9cf1ff80eb8b5f233b6ec887d7bd18b
hironoriyh <hironoriyh@gmail.com>
2019-09-11 18:59:56 +0900

    working

--- show_plot.py
+++ show_plot.py
@@ -3,17 +3,14 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
-
+import math
 
 def showplot(filenamem, saveimg=False):
     arr = np.load(filename)
     print("shape of input arr", arr.shape)
-
-    print(arr.shape)
     start_x= 8
     end_x = 15
-
-    print("max", np.max(arr[:, 0, start_x:end_x], axis=0)) # show max brightness for each x (y is fixed with 0)
+    # print("max", np.max(arr[:, 0, start_x:end_x], axis=0)) # show max brightness for each x (y is fixed with 0)
 
     #brightness
     N = arr.shape[0] #samples
@@ -31,10 +28,6 @@ def showplot(filenamem, saveimg=False):
 
     #frequency
     freq = np.linspace(0, 1/dt, arr.shape[0])
-    # freqList = np.fft.fftfreq(N, d=1.0/4000)
-    # print("freqList", freqList)
-    # print("freq", freq.shape)
-    # print(arr[:, 0, 2:5])
     F_xrow = []
     print(arr[:, 0].shape)
     for ind in np.arange(start_x, end_x):
@@ -43,6 +36,9 @@ def showplot(filenamem, saveimg=False):
         F[(freq >=fn)] = 0
         # F = np.abs(F)
         F_xrow.append(F)
+
+    print("shape of fft", F_xrow[0].shape)
+
 
     # plot
     plt.subplot(312)
@@ -69,6 +65,35 @@ def showplot(filenamem, saveimg=False):
     print("saved")
 
 
+def umetani(filename):
+
+    cap = cv2.VideoCapture(filename)
+    all_frame_nums  =  int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+
+
+
+
+    # arr = np.load(filename)
+    # print(arr.shape)
+    A = np.zeros((arr.shape[1], arr.shape[2]), dtype=np.complex)
+
+    while True:
+        frame = cap.read()[1]
+        A[:,:] += frame*(math.cos(2*math.pi/40) - 1j*math.sin(2*math.pi/40.0))
+
+    # for iframe in range(arr.shape[0]):
+    # # for iframe in range(2):
+    #     A[:,:] += arr[iframe,:,:]*(math.cos(2*math.pi/40) - 1j*math.sin(2*math.pi/40.0))
+    #     # print(A)
+    A /= arr.shape[0]
+    print(A.dtype)
+    plt.imshow(A.astype(np.uint8))
+    print(A[0, :10])
+    print(A.astype(np.uint8)[0, :10])
+    plt.show()
+    # print(A, A.shape)
+
 def drawimgs(arr):
     for img in arr:
         # img = cv2.
@@ -83,8 +108,12 @@ def drawimgs(arr):
 if __name__ == '__main__':
 
     print('sys.argv         : ', sys.argv)
-    filename = sys.argv[1]
-    print("npy name", filename[6:-4])
+    # filename = sys.argv[1]
+    # print("npy name", filename[6:-4])
     # filename="h80hz_x500_y500.npy"
     # print("video name", sys.argv[2])
-    showplot(filename, False)
+    # showplot(filename, False)
+
+    filename = sys.argv[1]
+    print("filename", filename[8:-4])
+    umetani(filename)
